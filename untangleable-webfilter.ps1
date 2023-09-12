07433a770f2ffcf40b2130eef884d60a01b460a1
returaxel <48722741+returaxel@users.noreply.github.com>
2023-09-12 22:43:31 +0200

    Update untangleable-webfilter.ps1

--- untangleable-webfilter.ps1
+++ untangleable-webfilter.ps1
@@ -35,20 +35,19 @@
 param (
     [Parameter()][string]$BlockListURL,
     [Parameter()][string]$FilePath = "$($ENV:OneDrive)\WebFilter.json",
-    [Parameter()][switch]$ShowAll,
+    [Parameter()][switch]$OutSkipped,
     [Parameter()][switch]$OutFile
 )
 
 function RegexMagic {
     param (
         [Parameter()][array]$BlockList,
-        # TestList: 41/41 matches, 2109 steps
-        [Parameter()][string]$Regex = '^(?:[^\w*-]*(?:\d+(?:\.\d+)+[^\w*-]+)?)([\w\-\*]+)(\.?[\w\.\-\*]*)(\.[\w\.\-\*]+)' 
+        [Parameter()][string]$Regex = '(?:^[\w]*\ )?(\b[\w\-\*]+)(\.?[\w\.\-\*]*)(\.[\w\.\-\*]+(?:[\W]*?$))'
     ) 
 
     $BlockList | ForEach-Object {
 
-        # Skip if empty or contain the following characters 
+        # Skip blocklist comments, edit regex as needed
         if (![string]::IsNullOrWhiteSpace($_) -and ($_ -notmatch '!|@|#')) {
 
             # Make object for output
@@ -72,24 +71,32 @@ function RegexMagic {
     
             $RegexMatch = [regex]::Matches($_, $Regex)
 
-            try {
-                $RegexMagic.SUB = $RegexMatch.Groups[1].Value # Sub domain
-                $RegexMagic.SLD = $RegexMatch.Groups[2].Value # Second level domain 
-                $RegexMagic.TLD = $RegexMatch.Groups[3].Value # Top level domain, everything after the last punctuation
-                $RegexMagic.FullMatch = '{0}{1}{2}' -f $RegexMatch.Groups[1],$RegexMatch.Groups[2],$RegexMatch.Groups[3]
-            }
-            catch {
-                Write-Host $RegexMagic.URL -ForegroundColor DarkYellow
-            } 
+            if (-not[string]::IsNullOrEmpty($RegexMatch)) {
 
-            $RegexMagic.WellFormed = [Uri]::IsWellFormedUriString($RegexMagic.FullMatch, 'Relative')
-            
-            if (![string]::IsNullOrEmpty($RegexMagic.FullMatch) -and ($RegexMagic.WellFormed)) {
-                $RegexMagic.string = $RegexMagic.FullMatch
+                try {
+                    $RegexMagic.SUB = $RegexMatch.Groups[1].Value # Sub domain
+                    $RegexMagic.SLD = $RegexMatch.Groups[2].Value # Second level domain 
+                    $RegexMagic.TLD = $RegexMatch.Groups[3].Value # Top level domain, everything after the last punctuation
+                    $RegexMagic.FullMatch = '{0}{1}{2}' -f $RegexMatch.Groups[1],$RegexMatch.Groups[2],$RegexMatch.Groups[3]
+                }
+                catch {
+                    Write-Host $RegexMagic.URL -ForegroundColor DarkYellow
+                } 
+
+                $RegexMagic.WellFormed = [Uri]::IsWellFormedUriString($RegexMagic.FullMatch, 'Relative')
+                
+                if ($OutFile) {
+                    if (![string]::IsNullOrEmpty($RegexMagic.FullMatch) -and ($RegexMagic.WellFormed)) {
+                        $RegexMagic.string = $RegexMagic.FullMatch
+                    }
+                }
+
                 return $RegexMagic
+
             }
+
         }         
-        elseif ($ShowAll) {
+        elseif ($OutSkipped) {
             Write-Host $_ -ForegroundColor DarkGray
         }
 
@@ -100,7 +107,7 @@ function RegexMagic {
 $RunTime = Measure-Command { # START MEASURE
 
 # Iterate list: might need tweaking depending on format
-[array] $BlockList = (Invoke-WebRequest $BlockListURL -UseBasicParsing).Content -split '\r?\n'
+[array] $BlockList = [System.Collections.Generic.List[string]] (Invoke-WebRequest $BlockListURL -UseBasicParsing).Content -split '\r?\n'
 
 $RegexMagic = RegexMagic -BlockList $BlockList
 
@@ -116,4 +123,6 @@ Write-Host "`n"
 Write-Host "`n Source: $($BlockList.Length) entries"
 Write-Host " Output: $($RegexMagic.Length) entries"
 Write-Host " RunTime: $($RunTime.TotalSeconds) seconds"
-Write-Host " FilePath: $($FilePath)`n"
+if ($OutFile) {
+    Write-Host " FilePath: $($FilePath)`n"
+}
